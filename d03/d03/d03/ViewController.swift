f84a41739aa8371dcf5fbe422aa0a7fb67e28f28
Inti FRANC-REGIS <ifranc-r@e2r7p14.42.fr>
2019-02-07 20:25:14 +0100

    start d03

---
+++ d03/d03/d03/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  d03
+//
+//  Created by Inti FRANC-REGIS on 2/7/19.
+//  Copyright © 2019 Inti FRANC-REGIS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var CollectionView: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
