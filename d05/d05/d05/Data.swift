de094f07c4d517b77ff5c50ce5a447e43c693d17
Inti FRANC-REGIS <ifranc-r@e3r12p10.42.fr>
2019-02-28 13:32:05 +0100

    icon change and data

---
+++ d05/d05/d05/Data.swift
@@ -0,0 +1,19 @@
+//
+//  Data.swift
+//  d05
+//
+//  Created by Inti FRANC-REGIS on 2/28/19.
+//  Copyright © 2019 Inti FRANC-REGIS. All rights reserved.
+//
+
+import Foundation
+
+
+struct Data {
+    static var locations : [(Double, Double, String, String)] =
+    [(48.8918717, 2.3216461, "Ecole 42", "France"),
+     (40.856061, 14.246392, "Pizzaria Starita", "Naples"),
+     (18.792020, 98.951796, "Pirate Drink", "Chiang Mai"),
+     (48.8918717, 2.3216461, "Ecole 42", "France")
+    ]
+}
