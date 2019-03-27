81e2704a322fc30036a80ca76fcd5972009cc7f8
Inti FRANC-REGIS <ifranc-r@e2r10p17.42.fr>
2019-03-27 14:56:34 +0100

    d00 and d02

--- d02/d02/d02/DataDeathList.swift
+++ d02/d02/d02/DataDeathList.swift
@@ -10,8 +10,8 @@ import Foundation
 
 struct Data {
     static var deathdata : [(String, Date, String)] = [
-        ("Coco Pops tretre tret", Date.from(year: 2020, month: 05, day: 30)!, "En faisant caca hhhih ooh o hoh o oh ohihoi hoi  \nhoi ho hioh oih oih oih oih oihoih oh h oih oih o"),
-        ("Mikey", Date.from(year: 2019, month: 04, day: 15)!, "En faisant prout"),
+        ("Coco Pops", Date.from(year: 2020, month: 05, day: 30)!, "En tombant dans un bol de lait périmé, puis machet par un enfant avec pleins debouton d'acnes moisis"),
+        ("Mikey", Date.from(year: 2019, month: 04, day: 15)!, "En chantant \"Paroles\" de dalida et Alain Delon "),
         ("Licorn", Date.from(year: 2021, month: 01, day: 11)!, "En miolant"),
         ("Le chat de la voisine", Date.from(year: 2021, month: 01, day: 12)!, "Etouffer par ses poils"),
     ]
